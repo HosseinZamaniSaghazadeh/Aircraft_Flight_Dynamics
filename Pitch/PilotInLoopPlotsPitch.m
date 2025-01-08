513c684ec464850b6d6c1bbfe9e2108dbf1fdd92
Hossein Zamani Saghazadeh <hosseinzamani1995@gmail.com>
2025-01-07 14:03:27 -1000

    First Edition

---
+++ Pitch/PilotInLoopPlotsPitch.m
@@ -0,0 +1,9 @@
+%% Pilot_In_Loop_Pitch_Angle_Plot
+
+%% Pitch Angle-Time Diagram
+
+figure('Name','Pitch Angle-Time Diagram')
+plot(Theta)
+title('Pitch Angle Variation due to Time')
+xlabel('Time (sec)')
+ylabel('Pitch Angle Theta (rad)')
